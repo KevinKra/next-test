e199e360ac8a547ce052353d2592c43f90c91b6a
kevinkra <kratok08@gmail.com>
2023-08-08 11:07:35 +1000

    re-install and re-configure tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,19 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    // <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    //   <Head>
-    //     <title>Create Next App</title>
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>My Shopping App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    //   <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-    //     render a product here.
-    //   </main>
+      <main className="flex text-red-400 flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        render a product here.
+      </main>
 
-    //   <footer className="flex items-center justify-center w-full h-24 border-t">
-    //     footer
-    //   </footer>
-    // </div>
-    <p>hello</p>
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        footer
+      </footer>
+    </div>
   );
 }
