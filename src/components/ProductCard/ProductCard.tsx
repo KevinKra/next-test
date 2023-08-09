13c9a6a1abf53d0f47fe588227030e9f01321486
kevinkra <kratok08@gmail.com>
2023-08-09 13:09:29 +1000

    add prettier tailwind, setup config, prettify Product tw styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -16,27 +16,41 @@ const ProductCard = ({ title, description, image, price }: IProductCard) => {
   return (
     <div
       className={`
-        relative 
-        border 
-        border-b-rose-400 
-        border-r-rose-400 
-        shadow-md 
-        grid 
-        grid-cols-2 
-        p-2 
+        relative
+        flex
+        flex-col 
         gap-4 
+        overflow-hidden 
+        border 
+        border-b-rose-500 
+        border-r-rose-500 
+        p-2 
         shadow-md 
-        overflow-hidden
+        lg:grid 
+        lg:grid-cols-2
       `}
     >
-      <div className="h-full border border-rose-400 border-2">
+      <div className="h-full border border-2 border-rose-500">
         <ProductImage noShadow width={500} title={title} image={image[0]} />
       </div>
-      <div className="bg-rose-400 h-full absolute -left-6 w-[8rem] z-[-1] skew-x-6" />
+      <div
+        className="
+        absolute 
+        top-0 
+        z-[-1] 
+        w-[20rem]
+        rotate-90 
+        skew-x-6 
+        bg-rose-500 
+        lg:-left-6 
+        lg:h-[25rem] 
+        lg:w-[8rem] 
+        lg:rotate-0"
+      />
       <div className="flex flex-col gap-y-4">
         <h4 className="w-1/5 text-xl font-bold">{title}</h4>
-        <p className="sm:hidden font-light w-4/5 line-clamp-3">{description}</p>
-        <div className="lowercase font-light">
+        <p className="line-clamp-3 w-4/5 font-light lg:hidden">{description}</p>
+        <div className="font-light lowercase">
           <p>
             <span className="font-medium">${price}</span>
           </p>
